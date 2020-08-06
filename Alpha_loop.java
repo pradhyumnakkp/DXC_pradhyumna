c80a6f7a38292290e89d65e20096f2c2daf070fb
pradhyumnakkp <68628501+pradhyumnakkp@users.noreply.github.com>
2020-08-06 23:14:36 +0530

    Create Alpha_loop.java
    .java

---
+++ Alpha_loop.java
@@ -0,0 +1,14 @@
+package dxc;
+
+public class Alpha_loop {
+
+	public static void main(String[] args) {
+		char a='A';
+		for(int i=0;i<26;i++)
+		{
+			System.out.println((char)a);
+			a++;
+		}
+	}
+
+}
