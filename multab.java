48362c4c83b18a900267519db1a215ca02b72c51
pradhyumnakkp <68628501+pradhyumnakkp@users.noreply.github.com>
2020-08-06 23:11:18 +0530

    Create multab.java

---
+++ multab.java
@@ -0,0 +1,16 @@
+package dxc;
+
+public class Multab {
+
+	public static void main(String[] args) {
+		int x=5;
+		for(int i=0;i<=10;i++)
+		{    
+			int a=x*i;
+			System.out.println(+x+"*"+i+"="+a);
+			
+		}
+
+	}
+
+}
