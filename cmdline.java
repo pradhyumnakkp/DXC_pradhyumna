0594f3e618a24580890e36b70a8080f29cf0de1c
pradhyumnakkp <68628501+pradhyumnakkp@users.noreply.github.com>
2020-08-06 23:13:28 +0530

    Create cmdline.java
    .java

---
+++ cmdline.java
@@ -0,0 +1,12 @@
+package dxc;
+
+public class cmddisp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("args-->"+args[0]);
+		System.out.println("args-->"+args[1]);
+
+	}
+
+}
